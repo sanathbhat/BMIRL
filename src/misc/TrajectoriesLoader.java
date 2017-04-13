9b10ef4821de213e1c7390a475c669503a16b3a5
sanathbhat <sanath.bhat10@gmail.com>
2017-04-13 02:47:23 -0400

    Fixed major bug in trajectories loader class

--- src/misc/TrajectoriesLoader.java
+++ src/misc/TrajectoriesLoader.java
@@ -43,25 +43,29 @@ public class TrajectoriesLoader {
                     String line;
                     List<StateActionPair> currentTrajectory = new ArrayList<>();
                     int m = 0;
-                    while((line = br.readLine()) != null) {
+                    int currentTaskSAPairNo = 0;
+                    main: while((line = br.readLine()) != null) {
                         String lineComp[] = line.split("\t");
                         double pos = Double.parseDouble(lineComp[0]);
                         int s = Integer.parseInt(lineComp[1]);
                         int a = Integer.parseInt(lineComp[2]);
 
-                        if(pos > trajectoryBoundaries[m]) {
+                        while(pos > trajectoryBoundaries[m]) {
                             //new task starts, add currentTrajectory to previous task trajectory set, iff it is a non-zero length trajectory
                             if(currentTrajectory.size() >0) {
                                 allTasksTrajectories.get(m).add(currentTrajectory);
                             }
                             //start new trajectory for new task
                             currentTrajectory = new ArrayList<>();
+                            currentTaskSAPairNo = 0;
                             m++;
                             if(m==trajectoryBoundaries.length)      //trajectories from m tasks have been read
-                                break;
+                                break main;
                         }
-                        if(m%10==0)         //load every 10th (s,a) pair
-                            currentTrajectory.add(new StateActionPair(s, a));                        
+                        if(currentTaskSAPairNo%10==0)         //load every 10th (s,a) pair
+                            currentTrajectory.add(new StateActionPair(s, a));   
+                        
+                        currentTaskSAPairNo++;
                     }
                     //if currentTrajectory still holds the last read task's trajectory, add it to its trajectory set
                     if(m<trajectoryBoundaries.length && currentTrajectory.size() >0) {
