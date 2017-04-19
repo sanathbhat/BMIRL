29abfc377799d87a2aad39edc73c2adf50482e24
sanathbhat <sanath.bhat10@gmail.com>
2017-04-19 05:58:01 -0400

    Added new samples processor that processes the samples file and computes the expected reward directly

---
+++ src/main/NewSamplesProcessor.java
@@ -0,0 +1,205 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import misc.Utilities;
+
+/**
+ * Processes samples file and generates expected reward at the same time
+ * @author sanat
+ */
+public class NewSamplesProcessor {
+
+    static final int NSTATES = 154;
+    static final int NACTIONS = 7;
+    static final int NTASKS = 3;
+    static final int CUTOFF = 5;
+
+    public static void main(String[] args) {
+        String samplesPath = "output/samples500KAveOverTraj.txt";
+        String prunedSamplesPath = "output/prunedsamples/" + new SimpleDateFormat("MM.dd.hh.mm").format(new Date()) + "/";
+        new File(prunedSamplesPath).mkdir();
+//        List<double[][]> prunedRewardSets = new ArrayList<>();
+//        List<Double> logWeights = new ArrayList<>();
+
+//      int lwPos = NSTATES * NACTIONS + 2 * NTASKS + 1;
+        int lwPos = 2 * NTASKS + 2;
+        
+        //two passes required as so much cannot be stored in memory
+        //Pass 1: find max log weight
+        int linesRead = 0;
+        double maxLW = Double.NEGATIVE_INFINITY;
+        try (BufferedReader br = new BufferedReader(new FileReader(samplesPath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] sample = line.split(" ");
+                if (Utilities.isValidDouble(sample[lwPos])) {
+                    double currLW = Double.parseDouble(sample[lwPos]);
+                    if (currLW > maxLW) {
+                        maxLW = currLW;
+                    }
+                }
+                linesRead++;
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Lines read before AIOOB, pass 1 = " + linesRead);
+        } catch (NumberFormatException nex) {
+            System.out.println("Lines read before NFE, pass 1 = " + linesRead);
+            nex.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //Pass 2: Find lse using all rewards within cutoff distance from maxLW reward
+        //HashMap<Double, RewardsCParamsSet> prunedRewardsDistribution = new HashMap<>();
+        //List<Map.Entry<Double, RewardsCParamsSet>> prunedRewardsDistributionList = new ArrayList<>();
+        double lse = 0;
+        linesRead = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(samplesPath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] sample = line.split(" ");
+                if (Utilities.isValidDouble(sample[lwPos])) {
+                    double currLW = Double.parseDouble(sample[lwPos]);
+                    if (maxLW - currLW < CUTOFF) {
+                        lse += Math.exp(currLW - maxLW);
+                    }
+                }
+
+                linesRead++;
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Lines read before AIOOB, pass 2 = " + linesRead);
+        } catch (NumberFormatException nex) {
+            nex.printStackTrace();
+            System.out.println("Lines read before NFE, pass 2 = " + linesRead);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        lse = maxLW + Math.log(lse);
+        //double Z = Math.exp(lse);
+        System.out.println("LSE ="+lse);
+        
+        //Pass 3: Compute expected reward
+        double expectedRF[][] = new double[NTASKS][NSTATES*NACTIONS];
+        linesRead = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(samplesPath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] sample = line.split(" ");
+                if (Utilities.isValidDouble(sample[lwPos])) {
+                    double currLW = Double.parseDouble(sample[lwPos]);
+                    if (maxLW - currLW < CUTOFF) {
+                        //int cursor = NSTATES * NACTIONS + 1;
+//                        int cursor = 2;
+                        double[][] rewards = new double[NTASKS][NSTATES * NACTIONS];
+//                        double[] cI = new double[NTASKS];
+//                        for (int i = 0; i < NTASKS; i++) {
+//                            cI[i] = Double.parseDouble(sample[cursor++]);
+//                        }
+                        int cursor = 5;
+                        double currWeight = Math.exp(currLW-lse);
+                        for (int m = 0; m < rewards.length; m++) {
+                            String taskMReward[] = sample[cursor + m].split(",");
+                            for (int r = 0; r < taskMReward.length; r++) {
+//                                rewards[i][j] = Double.parseDouble(r[j]);
+                                expectedRF[m][r] += (currWeight * Double.parseDouble(taskMReward[r]));
+                            }
+                        }
+
+                        //if (isValidDouble(sample[cursor])) {
+                        //prunedRewardsDistribution.put(Double.parseDouble(sample[cursor]), new RewardsCParamsSet(rewards, cI));
+                        //prunedRewardsDistributionList.add(new AbstractMap.SimpleEntry<>(Double.parseDouble(sample[cursor]), new RewardsCParamsSet(rewards, cI)));
+                        //writeToFile(prunedSamplesPath, rewards, cI, Double.parseDouble(sample[cursor]));
+                        
+//                    prunedRewardSets.add(rewards);
+//                    logWeights.add(Double.parseDouble(sample[70]));
+                        //}
+                    }
+                }
+
+                linesRead++;
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Lines read before AIOOB, pass 3 = " + linesRead);
+        } catch (NumberFormatException nex) {
+            nex.printStackTrace();
+            System.out.println("Lines read before NFE, pass 3 = " + linesRead);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        for (int m = 0; m < NTASKS; m++) {
+            for (int s = 0; s < NSTATES; s++) {
+                for (int a = 0; a < NACTIONS; a++) {
+                    System.out.print(expectedRF[m][s*NACTIONS+a] + "\t");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        
+        
+//        double maxLogWeight = prunedRewardsDistribution.keySet().stream().max(Double::compare).get();
+        //System.out.println(maxLogWeight);
+//        prunedRewardsDistribution.keySet()
+//                .stream()
+//                .parallel()
+//                .forEach(x -> {
+//                    //if(x-maxLogWeight>-10000) {
+//                    RewardsCParamsSet rcps = prunedRewardsDistribution.get(x);
+//                    writeToFile(prunedSamplesPath, rcps.getRewards(), rcps.getcParams(), x);
+//                    //}
+//                });
+        
+//        prunedRewardsDistributionList
+//                .stream()
+//                .parallel()
+//                .forEach(x -> {
+//                    RewardsCParamsSet rcps = x.getValue();
+//                    writeToFile(prunedSamplesPath, rcps.getRewards(), rcps.getcParams(), x.getKey());
+//                });
+
+    }
+
+    
+
+    private static void writeToFile(String prunedSamplesPath, double[][] rewardSet, double[] cParams, Double logWeight) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(prunedSamplesPath + logWeight + ".txt", true))) {
+            for (double[] reward : rewardSet) {
+                bw.append(formatReward(reward) + "\n");
+            }
+
+            bw.append(logWeight + "\n");
+            for (int i = 0; i < NTASKS; i++) {
+                bw.append(cParams[i] + "\t");
+            }
+//            bw.append(cParams[0]+ "\t");
+//            bw.append(cParams[1]+ "\t");
+//            bw.append(cParams[2]+ "\t");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String formatReward(double[] reward) {
+        String formatted = "";
+        for (int i = 0; i < NSTATES; i++) {
+            for (int j = 0; j < NACTIONS; j++) {
+                formatted += reward[i * NACTIONS + j] + "\t";
+            }
+            formatted += "\n";
+        }
+        return formatted;
+    }
+}
